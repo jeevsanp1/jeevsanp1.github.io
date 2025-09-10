85701c97ad9a84e96236add9c1bd6e2d88e30b77
jeevsanp1 <100105187+jeevsanp1@users.noreply.github.com>
2025-09-10 13:16:47 -0400

    Update nav

--- src/pages/nav.tsx
+++ src/pages/nav.tsx
@@ -3,10 +3,10 @@ import "../index.css";
 function Nav() {
   return (
     <>
-      <div className=" bg-teal-900 h-12 font-display">
+      <div className=" bg-teal-900 h-12 ">
         <ul className="flex flex-row pt-3 text-cyan-50">
           <li className="px-4">
-            <a href="/">Sanjeev Vijayakumar</a>
+            <a href="/">SV </a>
           </li>
           <li className="px-4">
             <a href="/#about">About</a>
