03b665edd6b3b7836f1dab7c953182ccdbca6d27
jeevsanp1 <100105187+jeevsanp1@users.noreply.github.com>
2025-09-10 13:12:58 -0400

    Homepage adjust

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,27 +11,24 @@ import Contact from "./contact";
 
 function Homepage() {
   const words1: any = [
-    "highschooler",
     "brother",
-    "electrical lead",
+    "engineer",
     "programmer",
     "guitarist",
-    "f1 fan",
     "learner",
     "tamilian",
     "michigander",
   ];
   return (
-    <div className="flex flex-col font-display">
+    <div className="flex flex-col">
       <div className="flex flex-col items-center w-full bg-emerald-200">
         <img
           className="size-64 pt-4 hover:animate-spin-slow"
           src={ImageName}
         ></img>
         <h1 className="pt-4 pb-12 text-2xl text-center w-2/5">
-          hi. if i were to describe myself in a word, i would say:
           <br />
-          i am a<FlipWords words={words1} />
+          i am a <FlipWords words={words1} />
         </h1>
         <button className="mb-2 p-0.5 relative inline-flex h-14 overflow-hidden rounded-full focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
